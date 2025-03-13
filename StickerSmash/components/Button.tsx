6415f0354bc3d13143640bfad32586562ad59fd7
giovana-cruzado <giovanarusbe@gmail.com>
2025-03-13 10:54:03 -0300

    Criando imagem dinâmica

--- StickerSmash/components/Button.tsx
+++ StickerSmash/components/Button.tsx
@@ -4,16 +4,17 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 type Props = {
   label: string;
   theme?: 'primary';
+  onPress?: () => void; 
 };
 
-export default function Button({ label, theme }: Props) {
+export default function Button({ label, theme, onPress }: Props) {
   if (theme === 'primary') {
     return (
       <View style={[styles.buttonContainer,
         { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
         ]}
         >
-        <Pressable style={[styles.button, { backgroundColor: '#fff' }]} onPress={() => alert("AHHHHHHH")}
+        <Pressable style={[styles.button, { backgroundColor: '#fff' }]} onPress={onPress}
         >
           <FontAwesome
             name="picture-o"
@@ -21,7 +22,9 @@ export default function Button({ label, theme }: Props) {
             color='#25292e'
             style={styles.buttonIcon}
           />
-          <Text style={styles.buttonLabel}>{label}</Text>
+          <Text style={[styles.buttonLabel, {color: "#25292e" }]}>
+            {label}
+          </Text>
         </Pressable>
       </View>
     );
