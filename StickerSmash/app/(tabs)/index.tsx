6e72a8bb75c471b1d3dbd4e4d765d37bf7ca3a3e
felipe-botura <felipebotura7@gmail.com>
2025-03-27 10:47:49 -0300

    Criando o modal de figurinhas.

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -5,6 +5,7 @@ import Button from "@/components/Button";
 import * as ImagePicker from 'expo-image-picker';
 import IconButton from "@/components/IconButton";
 import CircleButton from "@/components/CircleButton";
+import EmojiPicker from "@/components/EmojiPicker";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
@@ -13,6 +14,7 @@ export default function Index() {
     undefined
   );
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
 
   const pickImageAsync = async () => {
@@ -35,7 +37,11 @@ export default function Index() {
   }
 
   const onAddSticker = () => {
+    setIsModalVisible(true);
+  }
 
+  const onModalClose = () => {
+    setIsModalVisible(false);
   }
 
   const onSaveImageAsync = () => {
@@ -67,6 +73,9 @@ export default function Index() {
           />
         </View>
       )}
+      <EmojiPicker isVisible={isModalVisible}onClose={onModalClose}>
+
+      </EmojiPicker>
     </View>
   );
 }
