e8a470e40f050876cbeee338300b96c13a80febe
giovana-cruzado <giovanarusbe@gmail.com>
2025-06-12 10:34:46 -0300

    Personalizações finalizadas.

--- StickerSmash/app/(tabs)/_layout.tsx
+++ StickerSmash/app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 export default function TabLayout() {
   return (
     <Tabs screenOptions={{
-      tabBarActiveTintColor: "#ffd33d",
+      tabBarActiveTintColor: "#fff",
       headerStyle: {
         backgroundColor: "#2e1b38",
       },
